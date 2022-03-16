c048bd9fddb5186402d0786753e224f46c080e92
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-16 01:31:02 -0300

    add response to api and default seed

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
 
 createServer({
-  routes() {
-    this.namespace = 'api';
+  models: {
+    transaction: Model,
+  },
 
-    this.get('/transactions', () => {
-      return [
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
+          title: "Primeira transação",
+          type: "deposit",
+          category: "Dev",
+          amount: 0,
+          createdAt: new Date(),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
