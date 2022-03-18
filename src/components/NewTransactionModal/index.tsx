4e218e27b4c9cc36a5bc008d7a0a34922eeb7a1a
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-18 00:30:48 -0300

    reset modal when send form to api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -25,15 +25,22 @@ export function NewTransactionModal({
 
   const { createTransaction } = useContext(TransactionsContext);
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     });
+
+    setTitle("");
+    setAmount(0);
+    setType("deposit");
+    setCategory("");
+
+    onRequestClose();
   }
 
   return (
